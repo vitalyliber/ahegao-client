1e6b25a660684cdf19ff0106d88fb9a05c649d90
Vitaly Liber <zenamax@gmail.com>
2019-12-08 17:26:46 +0500

    lazy loading for images was added

---
+++ components/Post.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Link from "next/link";
+import { Waypoint } from "react-waypoint";
+import dayjs from "dayjs";
+import React from "react";
+
+function Post({ el }) {
+  const generateSvg = (w, h) =>
+    `data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='${w}' height='${h}' ><rect width='${w}' height='${h}'/></svg>`;
+  const [avatar, setAvatar] = useState(generateSvg(50, 50));
+  const [image, setImage] = useState(
+    generateSvg(el.metadata.width, el.metadata.height)
+  );
+  return (
+    <Waypoint
+      onEnter={() => {
+        setImage(el.image);
+        setAvatar(el.user_avatar);
+      }}
+    >
+      <div className="col-sm-12 col-md-6" key={el.id}>
+        <div className="d-flex align-items-center mb-2 justify-content-between">
+          <Link href="/user/[pid]" as={`/user/${el.user_id}`}>
+            <div className="d-flex align-items-center">
+              <img
+                height="35"
+                width="35"
+                className="rounded-circle"
+                src={avatar}
+                alt={`Ahegao face from ${el.username}`}
+              />
+              <p className="mb-0 ml-2 font-weight-bold">{el.username}</p>
+            </div>
+          </Link>
+          <div className="text-black-50 d-inline">
+            {dayjs(el.updated_at).fromNow()}
+          </div>
+        </div>
+        <Link href="/post/[pid]" as={`/post/${el.id}`}>
+          <img className="img-fluid mb-3" src={image} alt="Ahegao face" />
+        </Link>
+      </div>
+    </Waypoint>
+  );
+}
+
+export default Post;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Link from "next/link";
 import Head from "next/head";
 import Axios from "axios";
 import dayjs from "dayjs";
@@ -7,6 +6,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 import "../styles.scss";
 import Nav from "../components/nav";
+import Post from "../components/Post";
 import useScrollRestoration from "../components/useScrollRestoration";
 
 let cache = {};
@@ -36,36 +36,7 @@ const Home = props => {
       <div className="container">
         <div className="row">
           {products.map(el => {
-            return (
-              <div className="col-sm-12 col-md-6" key={el.id}>
-                <div className="d-flex align-items-center mb-2 justify-content-between">
-                  <Link href="/user/[pid]" as={`/user/${el.user_id}`}>
-                    <div className="d-flex align-items-center">
-                      <img
-                        height="35"
-                        width="35"
-                        className="rounded-circle"
-                        src={el.user_avatar}
-                        alt={`Ahegao face from ${el.username}`}
-                      />
-                      <p className="mb-0 ml-2 font-weight-bold">
-                        {el.username}
-                      </p>
-                    </div>
-                  </Link>
-                  <div className="text-black-50 d-inline">
-                    {dayjs(el.updated_at).fromNow()}
-                  </div>
-                </div>
-                <Link href="/post/[pid]" as={`/post/${el.id}`}>
-                  <img
-                    className="img-fluid mb-3"
-                    src={el.image}
-                    alt="Ahegao face"
-                  />
-                </Link>
-              </div>
-            );
+            return <Post el={el} />;
           })}
         </div>
         <button
