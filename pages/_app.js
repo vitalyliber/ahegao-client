33abd0e8982e139fd645a9375ee747a80e9c934d
Vitaly Liber <zenamax@gmail.com>
2019-12-12 17:27:07 +0500

    scroll up button was added

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import App from 'next/app'
+import React from "react";
+import App from "next/app";
+import ScrollUpButton from "react-scroll-up-button";
 
 import "../styles.scss";
 
-
 class MyApp extends App {
   render() {
-    const { Component, pageProps } = this.props
-    return <Component {...pageProps} />
+    const { Component, pageProps } = this.props;
+    return (
+      <>
+        <Component {...pageProps} />
+        <ScrollUpButton />
+      </>
+    );
   }
 }
 
-export default MyApp
+export default MyApp;
