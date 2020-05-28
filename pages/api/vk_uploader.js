c8cef4bb22589266aff96a77baa01480986d108e
Vitaly Liber <zenamax@gmail.com>
2020-05-29 00:49:25 +0300

    fixed

--- pages/api/vk_uploader.js
+++ pages/api/vk_uploader.js
@@ -3,8 +3,15 @@ const group_id = process.env.VK_GROUP_ID;
 const token = process.env.VK_ACCESS_TOKEN;
 
 export default async (req, res) => {
+  const { links, server_token } = req.body;
+  if (server_token !== process.env.SERVER_TOKEN) {
+    res.statusCode = 401;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ error: "Server token error" }));
+    return;
+  }
   const listPhotos = await Promise.all(
-    req.body.map(link => vkPreparePhotoForPosting(link))
+    links.map(link => vkPreparePhotoForPosting(link))
   );
   const publishResponse = await fetch(
     `https://api.vk.com/method/wall.post?owner_id=-${group_id}&attachments=${listPhotos}&access_token=${token}&v=5.101`
